5e1f8df6592c11f12b4089d9b316e2c8856cda5a
David Fink <dfink.mmt-b2022@fh-salzburg.ac.at>
2023-12-02 14:54:04 +0100

    Day2: Part1/Part2 completed

---
+++ Day2/Part1/Program.cs
@@ -0,0 +1,53 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+int round = 1;
+int result = 0;
+
+foreach (string l in lines)
+{
+    string[] gameRounds = l.Split(';');
+
+    bool roundCorrect = true;
+    foreach(string s in gameRounds)
+    {
+        int greenAmount = getCountOfColor(s, "green");
+        int redAmount = getCountOfColor(s, "red");
+        int blueAmount = getCountOfColor(s, "blue");
+
+        if(greenAmount > 13 || redAmount > 12 || blueAmount > 14)
+        {
+            roundCorrect = false;
+            break;
+        }
+    }
+
+    if(roundCorrect)
+    {
+        result += round;
+    }    
+    round++;
+}
+
+Console.WriteLine("Result: " + result);
+
+int getCountOfColor(string line, string color)
+{
+    List<int> drops = new List<int>();
+
+    for (int i = 0; i < line.Length - 1; i++)
+    {
+        if(line[i] == color[0] && line[i+1] == color[1] && line[i+2] == color[2])
+        {
+            if(char.IsDigit(line[i - 3]))
+            {
+                return int.Parse(line[i - 3].ToString() + line[i - 2].ToString());
+            }
+            else
+            {
+                return int.Parse(line[i - 2].ToString());
+            }
+        }
+    }
+
+    return 0;
+}
---
+++ Day2/Part2/Program.cs
@@ -0,0 +1,60 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+int result = 0;
+
+foreach (string l in lines)
+{
+    string[] gameRounds = l.Split(';');
+
+    int minGreen = 0;
+    int minRed = 0;
+    int minBlue = 0;
+
+    foreach(string s in gameRounds)
+    {
+        int greenAmount = getCountOfColor(s, "green");
+        int redAmount = getCountOfColor(s, "red");
+        int blueAmount = getCountOfColor(s, "blue");
+
+        if(greenAmount > minGreen)
+        {
+            minGreen = greenAmount;
+        }
+
+        if(redAmount > minRed)
+        {
+            minRed = redAmount;
+        }
+
+        if(blueAmount > minBlue)
+        {
+            minBlue = blueAmount;
+        }
+    }
+
+    result += minGreen * minRed * minBlue;
+}
+
+Console.WriteLine("Result: " + result);
+
+int getCountOfColor(string line, string color)
+{
+    List<int> drops = new List<int>();
+
+    for (int i = 0; i < line.Length - 1; i++)
+    {
+        if(line[i] == color[0] && line[i+1] == color[1] && line[i+2] == color[2])
+        {
+            if(char.IsDigit(line[i - 3]))
+            {
+                return int.Parse(line[i - 3].ToString() + line[i - 2].ToString());
+            }
+            else
+            {
+                return int.Parse(line[i - 2].ToString());
+            }
+        }
+    }
+
+    return 0;
+}
