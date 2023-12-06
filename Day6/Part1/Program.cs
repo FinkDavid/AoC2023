e30897fbab41bcf88dd8553561ada6f302289da0
David Fink <dfink.mmt-b2022@fh-salzburg.ac.at>
2023-12-06 09:46:39 +0100

    Day6: Part1/Part2 completed

---
+++ Day6/Part1/Program.cs
@@ -0,0 +1,59 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+List<int> times = new List<int>();
+List<int> distances = new List<int>();
+
+Dictionary<int, int> records = new Dictionary<int, int>();
+
+string[] splitTimesString = lines[0].Split(':');
+string[] timesString = splitTimesString[1].Split(' ');
+times = GetNumberList(timesString);
+
+string[] splitDistancesString = lines[1].Split(':');
+string[] distancesString = splitDistancesString[1].Split(' ');
+distances = GetNumberList(distancesString);
+
+for(int i = 0; i < times.Count; i++)
+{
+    records.Add(times[i], distances[i]);
+}
+
+List<int> timesBeatenPerRun = new List<int>();
+
+foreach(KeyValuePair<int, int> record in records)
+{
+    int timesBeaten = 0;
+    for(int i = 0; i < record.Key; i++)
+    {
+        int distance = i * (record.Key - i);
+        if(distance > record.Value)
+        {
+            timesBeaten++;
+        }
+    }
+    timesBeatenPerRun.Add(timesBeaten);
+}
+
+int result = timesBeatenPerRun[0];
+for(int i = 1; i < timesBeatenPerRun.Count; i++)
+{
+    result *= timesBeatenPerRun[i];
+}
+
+Console.WriteLine("Result: " + result);
+
+List<int> GetNumberList(string[] numbers)
+{
+    List<int> numberList = new List<int>();
+
+    foreach(string number in numbers)
+    {
+        int i = 0;
+        if(int.TryParse(number, out i))
+        {
+            numberList.Add(i);
+        }
+    }
+
+    return numberList;
+}
---
+++ Day6/Part2/Program.cs
@@ -0,0 +1,19 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+string[] splitTimesString = lines[0].Split(':');
+long time = long.Parse(splitTimesString[1].Replace(" ", ""));
+
+string[] splitDistancesString = lines[1].Split(':');
+long distance = long.Parse(splitDistancesString[1].Replace(" ", ""));
+
+int timesBeaten = 0;
+for(long i = 0; i < time; i++)
+{
+    long currentDistance = i * (time - i);
+    if(currentDistance > distance)
+    {
+        timesBeaten++;
+    }
+}
+
+Console.WriteLine("Result: " + timesBeaten);
