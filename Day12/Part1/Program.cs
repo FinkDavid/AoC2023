a1c2f06edd84a9f392eafc3487705f59c5d1fbfa
David Fink <dfink.mmt-b2022@fh-salzburg.ac.at>
2023-12-12 07:19:14 +0100

    Day12: Part1 completed

---
+++ Day12/Part1/Program.cs
@@ -0,0 +1,101 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+int result = 0;
+
+foreach(string l in lines)
+{
+    string[] split = l.Split(' ');
+
+    string records = split[0];
+    string[] num = split[1].Split(',');
+    List<int> numbers = new List<int>();
+    for(int i = 0; i < num.Length; i++)
+    {
+        numbers.Add(int.Parse(num[i]));
+    }
+
+    int amountOfUnknown = 0;
+    foreach(char c in records)
+    {
+        if(c == '?')
+        {   
+            amountOfUnknown++;
+        }
+    }
+
+    int numberOfPossibilities = (int)Math.Pow(2, amountOfUnknown);
+    for(int i = 0; i < numberOfPossibilities; i++)
+    {
+        string combination = GetCombination(records, i, amountOfUnknown);
+        List<int> sizes = GetSegmentSizes(combination);
+        
+        
+        if(sizes.Count == numbers.Count)
+        {
+            bool isValidArragnment = true;
+            for(int j = 0; j < numbers.Count; j++)
+            {
+                if(numbers[j] != sizes[j])
+                {
+                    isValidArragnment = false;
+                    break;
+                }
+            }
+
+            if(isValidArragnment)
+            {
+                result++;
+            }
+        }
+    }
+}
+
+Console.WriteLine("Result: " + result);
+
+string GetCombination(string str, int value, int amountOfUnknown)
+{
+    char[] combination = str.ToCharArray();
+    int mask = 1;
+
+    for(int i = 0; i < str.Length; i++)
+    {
+        if(combination[i] == '?')
+        {
+            combination[i] = (value & mask) == 0 ? '#' : '.';
+            mask <<= 1;
+
+            if(amountOfUnknown-- == 0)
+            {
+                break;
+            }
+        }
+    }
+
+    return new string(combination);
+}
+
+List<int> GetSegmentSizes(string combination)
+{
+    List<int> segmentSizes = new List<int>();
+    int currentSegmentSize = 0;
+
+    foreach(char c in combination)
+    {
+        if(c == '#')
+        {
+            currentSegmentSize++;
+        }
+        else if(currentSegmentSize > 0)
+        {
+            segmentSizes.Add(currentSegmentSize);
+            currentSegmentSize = 0;
+        }
+    }
+
+    if(currentSegmentSize > 0)
+    {
+        segmentSizes.Add(currentSegmentSize);
+    }
+
+    return segmentSizes;
+}
---
+++ Day12/Part2/Program.cs
@@ -0,0 +1,44 @@
+﻿string[] lines = File.ReadAllLines("../input.txt");
+
+int result = 0;
+
+foreach(string l in lines)
+{
+    string[] split = l.Split(' ');
+
+    string records = split[0];
+    string extendedRecords = records;
+    string[] num = split[1].Split(',');
+    List<int> numbers = new List<int>();
+    for(int j = 0; j < 5; j++)
+    {
+        for(int i = 0; i < num.Length; i++)
+        {
+            numbers.Add(int.Parse(num[i]));
+        }
+    }
+
+    for(int j = 0; j < 4; j++)
+    {
+        extendedRecords = extendedRecords + "?" + records;
+    }
+
+    Console.Write(extendedRecords + " - ");
+
+    foreach(int size in numbers)
+    {
+        Console.Write(size + ",");
+    }
+    Console.WriteLine();
+
+    int amountOfUnknown = 0;
+    foreach(char c in extendedRecords)
+    {
+        if(c == '?')
+        {   
+            amountOfUnknown++;
+        }
+    }
+}
+
+Console.WriteLine("Result: " + result);
